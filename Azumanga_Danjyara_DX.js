12686795589fc35cfa4fe288414a0b6140bdb6a5
Eliawesomesauce <eli.bockus@gmail.com>
2025-06-22 21:47:59 -0500

    added fellas

--- Azumanga_Danjyara_DX.js
+++ Azumanga_Danjyara_DX.js
@@ -1,7 +1,20 @@
-function dealCard(){
-    const newCard = document.createElement("img");
-newCard.setAttribute("src","img/Pixle Osaka Tile.png")
+const decklist = [
+  "img/Pixle Chiyo Tile.png",
+  "img/Pixle Osaka Tile.png",
+  "img/Pixle Sakaki Tile.png",
+  "img/Pixle Tomo Tile.png",
+  "img/Pixle Yomi Tile.png"
+];
 
-const playersHand = document.getElementById("playersHand");
-playersHand.appendChild(newCard);
+function dealCard() {
+  const newCard = document.createElement("img");
+  newCard.src = getRandomCard(); // Set the image source
+
+  const playersHand = document.getElementById("playersHand");
+  playersHand.appendChild(newCard); // Add to the player's hand
+}
+
+function getRandomCard() {
+  const index = Math.floor(Math.random() * decklist.length);
+  return decklist[index];
 }
