89bdde03c22e680ec02d80c9c8bdc7427a874fd3
Eliawesomesauce <eli.bockus@gmail.com>
2025-06-27 20:19:42 -0500

    yea

--- Azumanga_Danjyara_DX.js
+++ Azumanga_Danjyara_DX.js
@@ -12,36 +12,15 @@ let playerCardsList = [
 document.getElementById("playersHand").addEventListener("click", function(event) {
   if (event.target.tagName === "IMG") {
     event.target.remove();
-    renderCards()
   }
 });
 
-function renderCards() {
-  const hand = document.getElementById('playersHand');
-  hand.innerHTML = ''; // Clear the hand
-
-  playerCardsList.forEach((card) => {
-    const img = document.createElement('img');
-    img.src = 'img/' + card.image;
-    img.classList.add('card');
-
-    // Use a reference to the card object, not the index
-    img.addEventListener('click', () => {
-      // Find the card's actual index in the array
-      const index = playerCardsList.indexOf(card);
-      if (index !== -1) {
-        playerCardsList.splice(index, 1); // Remove from array
-        renderCards(); // Re-render UI
-      }
-    });
-
-    hand.appendChild(img);
-  });
-}
 
 function checkDealCard(){
   if(playerCardsList.length<8){
     dealCard()
+  }else if(playerCardsList.length<9){
+    dealNextCard()
   }
 }
 
@@ -54,6 +33,15 @@ function dealCard() {
 
   playerCardsList.push(newCard)
   console.log(playerCardsList)
+}
+
+function dealNextCard() {
+const newCard = document.createElement("img")
+newCard.src = getRandomCard()
+const nextCard = document.getElementById("nextCard")
+nextCard.appendChild(newCard)
+playerCardsList.push(newCard)
+console.log(playerCardsList)
 }
 
 function getRandomCard() {
